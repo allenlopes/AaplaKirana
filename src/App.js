b804707ec5b7a150deec096c86eba905d51876c0
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-21 16:52:17 +0530

    In src/App.js files were modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Home from './pages/HomePage/Home'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<div>
+          <h1>404 Not Found :(</h1>
+        </div>} />
       </Routes>
       </BrowserRouter>
     </div>
