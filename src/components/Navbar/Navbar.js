18841d87307bc7b919cf7a88a30c07e4629ac832
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-21 21:00:42 +0530

    In src/components/Navbar/Navbar.js file was modified by creating the Navigation bar which has categories tab, etc..

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,36 +4,72 @@ import logo from '../../assets/logo.jpeg'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FiShoppingCart} from 'react-icons/fi'
 import {HiOutlineUser} from 'react-icons/hi'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const Navbar = () => {
     const [cartquantity, setcartquantity] = useState(0)
   return (
     <nav>
-        <div className='s1'>
-            <img src={logo} alt='logo' className='logo'/>
+      <div className="s1">
+        <img src={logo} alt="logo" className="logo" />
 
-            <div className='searchbar'>
-                <input type='text'
-                placeholder='Search for Products'
-                className='search' />
-                <button> <AiOutlineSearch/> </button>
-            </div>
-
-            <div className='right'>
-                <div className='cart'>
-                    <span className='qty'>{cartquantity} </span>
-                    <FiShoppingCart/>
-                </div>
-                <HiOutlineUser/>
-            </div>
+        <div className="searchbar">
+          <input
+            type="text"
+            placeholder="Search for Products"
+            className="search"
+          />
+          <button>
+            {" "}
+            <AiOutlineSearch />{" "}
+          </button>
         </div>
 
-
-        <div className='s2'>
-
+        <div className="right">
+          <div className="cart">
+            <span className="qty">{cartquantity} </span>
+            <FiShoppingCart />
+          </div>
+          <HiOutlineUser />
         </div>
+      </div>
+
+{/* The following class 's2' is used as Navigation bar which conatins - categories tab, About us tab, Contact us tab and More tab*/}
+      <div className="s2">
+        {/* The following Dropdown code has been taken from "react bootstrap dropdown" code from their official website and edited*/}
+        <Dropdown>
+          <Dropdown.Toggle variant="" id="dropdown-basic">
+            Categories
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Fresh Vegetables</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Fresh Fruits</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              Household Requirements
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <a>About Us</a>
+
+        <a>Contact Us</a>
+
+        <Dropdown>
+          <Dropdown.Toggle variant="" id="dropdown-basic">
+            More
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">FAQ</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Privacy Policy</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              Terms & Conditions
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
