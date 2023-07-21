1acd0fdc823886dc8648ea46d19a5da071bc6586
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-21 17:51:58 +0530

    In src/components/Navbar/Navbar.js file was modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.jpeg'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FiShoppingCart} from 'react-icons/fi'
 
 const Navbar = () => {
+    const [cartquantity, setcartquantity] = useState(0)
   return (
-    <div>Navbar</div>
+    <nav>
+        <div className='s1'>
+            <img src={logo} alt='logo' className='logo'/>
+
+            <div className='searchbar'>
+                <input type='text'
+                placeholder='Search for Products'
+                className='search' />
+                <button> {AiOutlineSearch} </button>
+            </div>
+
+            <div className='right'>
+                <div className='cart'>
+                    <span className='qty'>{cartquantity} </span>
+                    {FiShoppingCart}
+                </div>
+            </div>
+        </div>
+
+
+        <div className='s2'>
+
+        </div>
+    </nav>
   )
 }
 
