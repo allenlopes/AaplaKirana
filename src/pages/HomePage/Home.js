6cb9987d2a031a363c87d4dd7342c24b06b872d5
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-22 13:35:52 +0530

    In src/pages/HomePage/Home.js file was modified by importing BannerSlider.js

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import BannerSlider from '../../components/Banners/BannerSlider'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <BannerSlider />
         Home</div>
   )
 }
