5f5a6c5ec744b15ad66660044f95d36daec60280
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-22 16:54:37 +0530

    In src/components/Category files were created and modified

---
+++ src/components/Category/HomeCategories.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './HomeCategories.css'
+import img1 from '../../assets/Images/1.png'
+import img2 from '../../assets/Images/2.png'
+import img3 from "../../assets/Images/3.png";
+import img4 from "../../assets/Images/4.png";
+
+
+const HomeCategories = () => {
+  return (
+    <div className="homecategories">
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+      <div className="container"></div>
+    </div>
+  );
+}
+
+export default HomeCategories
