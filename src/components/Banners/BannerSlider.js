053c44638e622ed43c0b9a459dd8923c7cc2429b
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-22 15:25:39 +0530

    In src/components/Banners/BannerSlider.js file was modified

--- src/components/Banners/BannerSlider.js
+++ src/components/Banners/BannerSlider.js
@@ -6,17 +6,19 @@ const BannerSlider = () => {
     const data = [
       {
         id: 1,
-        image: "https://unsplash.com/photos/D6Tu_L3chLE",
+        image:
+          "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
         title:
           "Fresh Vegetables, Fruits & your daily household needs at your doorstep",
         description:
-          "We deliver fresh vegetables, fruits & your daily household needs within minutes you order them!",
+          "From Cherries to Berries, from Milk to Ghee, In our Grocery shop, there's plenty to see!",
         button: "https://www.google.com",
       },
 
       {
         id: 2,
-        image: "https://unsplash.com/photos/D6Tu_L3chLE",
+        image:
+          "https://images.unsplash.com/photo-1646668947240-a154d4d6c766?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
         title:
           "Fresh Vegetables, Fruits & your daily household needs at your doorstep",
         description:
@@ -43,7 +45,7 @@ const BannerSlider = () => {
                         <img src={item.image} alt='noimg' />
                         <div className='content'>
                             <h1>{item.title}</h1>
-                            <p>{item.description}</p>
+                            <span>{item.description}</span>
                             <button>Shop More</button>
                         </div>
                     </div>
