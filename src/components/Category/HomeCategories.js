0db042bde4c75651a4db66a6e5732a8ec0dee9c9
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-23 00:04:07 +0530

    In src/components/Category/HomeCategories.js file was modified

--- src/components/Category/HomeCategories.js
+++ src/components/Category/HomeCategories.js
@@ -5,14 +5,41 @@ import img2 from '../../assets/Images/2.png'
 import img3 from "../../assets/Images/3.png";
 import img4 from "../../assets/Images/4.png";
 
-
 const HomeCategories = () => {
   return (
     <div className="homecategories">
-      <div className="container"></div>
-      <div className="container"></div>
-      <div className="container"></div>
-      <div className="container"></div>
+      <div className="container">
+        <img src={img1} alt="img1" />
+        <div className="content">
+          <h1>Vegetables at your doorstep</h1>
+          <p>Shop Vegetables Now!</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <img src={img2} alt="img2" />
+        <div className="content">
+          <h1>Vegetables at your doorstep</h1>
+          <p>Shop Vegetables Now!</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <img src={img3} alt="img3" />
+        <div className="content">
+          <h1>Vegetables at your doorstep</h1>
+          <p>Shop Vegetables Now!</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <img src={img4} alt="img4" />
+        <div className="content">
+          <h1>Vegetables at your doorstep</h1>
+          <p>Shop Vegetables Now!</p>
+        </div>
+      </div>
+
     </div>
   );
 }
