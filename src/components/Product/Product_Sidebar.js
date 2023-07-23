635c0a027253c976c0a3855ae346cdd2d98efe5f
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-23 14:34:55 +0530

    In src/components/Product folder was created

---
+++ src/components/Product/Product_Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CategorySidebar from './CategorySidebar'
+import AllProduct from './AllProduct'
+
+const Product_Sidebar = () => {
+  return (
+    <div>
+        <CategorySidebar />
+        <AllProduct />
+    </div>
+  )
+}
+
+export default Product_Sidebar
