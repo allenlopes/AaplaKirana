2c740a63d36f839cc0cd041a57a0d6cb03143353
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-23 16:25:06 +0530

    In src/components/Product/AllProductCard.js file was modified

--- src/components/Product/AllProductCard.js
+++ src/components/Product/AllProductCard.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import {AiOutlineEye} from 'react-icons/ai'
+import {AiOutlinePlusCircle} from 'react-icons/ai'
 import './AllProductCard.css'
 
-const AllProductCard = ({data}) => {
+const AllProductCard = ({ data }) => {
   return (
     <div className="product">
-      <div className="s1"></div>
-      <div className="s2"></div>
-      <div className="s3"></div>
+
+      <div className="s1">
+        <img src={data.productimage} alt={"no image"} />
+      </div>
+
+      <div className="s2">
+        <h3>
+          {
+          data.productprice - (data.productprice * data.discountpercent / 100)
+          }
+          <span>&#8377;{data.productprice}</span>
+        </h3>
+      </div>
+
+      <div className="s3">
+        <p>{data.counttype}</p>
+      </div>
+
+      <div className="addbtn">
+        <AiOutlineEye />
+        <AiOutlinePlusCircle />
+      </div>
     </div>
   );
 }
