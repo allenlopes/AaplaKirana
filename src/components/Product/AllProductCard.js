b4b4ac6231aa3b9a63649f04f8564b5e6aa4bddf
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-23 16:11:02 +0530

    In src/components/Product/AllProductCard.js file was modified

---
+++ src/components/Product/AllProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AllProductCard.css'
+
+const AllProductCard = ({data}) => {
+  return (
+    <div className="product">
+      <div className="s1"></div>
+      <div className="s2"></div>
+      <div className="s3"></div>
+    </div>
+  );
+}
+
+export default AllProductCard
