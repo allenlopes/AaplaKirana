02c2178571626a1fd900f8a01f33e5c7e8380412
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-23 14:36:54 +0530

    In src/pages/HomePage/Home.js file was modified

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import BannerSlider from '../../components/Banners/BannerSlider'
 import HomeCategories from '../../components/Category/HomeCategories'
+import Product_Sidebar from '../../components/Product/Product_Sidebar'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar />
         <BannerSlider />
         <HomeCategories />
+        <Product_Sidebar />
         Home</div>
   )
 }
