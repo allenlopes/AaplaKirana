7336520a5eecfe741fd5349b52a115b54ab12702
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-25 00:03:21 +0530

    In src/components/Footer/Footer1.js file modified

--- src/components/Footer/Footer1.js
+++ src/components/Footer/Footer1.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import veges from '../../assets/veges.png'
+import './Footer1.css'
 
 const Footer1 = () => {
   return (
-    <div className='footer1'>
-        <div className='left'></div>
-        <div className='right'></div>
+    <div className="footer1">
+      <div className="left">
+        <img src={veges} alt="veges" />
+      </div>
+      <div className="right">
+        <h1>Fresh Vegetables & Fruits at your doorstep!</h1>
+        <p>
+          We deliver fresh Vegetables & fruits at your doorstep, We deliver
+          fresh Vegetables & fruits at your doorstep
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer1
