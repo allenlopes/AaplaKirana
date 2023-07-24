d88d2410c0830142fd30adceb1fbc0dac3641eb1
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-24 23:59:41 +0530

    In src/components/Footer/Footer1.js file modified

---
+++ src/components/Footer/Footer1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer1 = () => {
+  return (
+    <div className='footer1'>
+        <div className='left'></div>
+        <div className='right'></div>
+    </div>
+  )
+}
+
+export default Footer1
