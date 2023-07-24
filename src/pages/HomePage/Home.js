231db1700b6391416f2722736b10a8a85aa78a96
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-24 23:56:07 +0530

    In src/pages/HomePage/Home.js file modified

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -3,6 +3,8 @@ import Navbar from '../../components/Navbar/Navbar'
 import BannerSlider from '../../components/Banners/BannerSlider'
 import HomeCategories from '../../components/Category/HomeCategories'
 import Product_Sidebar from '../../components/Product/Product_Sidebar'
+import Footer1 from '../../components/Footer/Footer1'
+import Footer2 from '../../components/Footer/Footer2'
 
 const Home = () => {
   return (
@@ -11,7 +13,9 @@ const Home = () => {
         <BannerSlider />
         <HomeCategories />
         <Product_Sidebar />
-        Home</div>
+        <Footer1 />
+        <Footer2 />
+    </div>
   )
 }
 
