0d7e645232fc712f917ac8a45d4d2aa957189cad
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-26 23:57:21 +0530

    In src/components/Footer/Footer2.js file modified

--- src/components/Footer/Footer2.js
+++ src/components/Footer/Footer2.js
@@ -4,7 +4,9 @@ const Footer2 = () => {
   return (
     <div className="footer">
       <div className="footerin1"></div>
-      <div className="footerin2"></div>
+      <div className="footerin2">
+        <h3>© Copyright 2023 AllenGrocery, Inc. All rights Reserved.</h3>
+      </div>
     </div>
   );
 }
