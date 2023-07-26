ff2ca7801aab553dc72c24b619f62d0cd2e15a34
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-27 00:18:00 +0530

    In src/components/Footer/Footer2.js file modified

--- src/components/Footer/Footer2.js
+++ src/components/Footer/Footer2.js
@@ -2,11 +2,13 @@ import React from 'react'
 import payimg from '../../assets/pay.png';
 import logo from '../../assets/logo.jpeg';
 import social from '../../assets/social.png';
+import {HiOutlineMail} from 'react-icons/hi';
+import {AiOutlineSend} from 'react-icons/ai';
+import './Footer2.css';
 
 const Footer2 = () => {
   return (
     <div className="footer">
-
       <div className="footerin1">
         <div className="f1">
           <img src={logo} alt="logo" className="logo" />
@@ -45,10 +47,18 @@ const Footer2 = () => {
 
         <div className="f2">
           <h3>Subscribe Now</h3>
+          <p>Subscribe your email for newsletter and etc.</p>
+          <div className="inputcontainer">
+            <div className="icon">
+              <HiOutlineMail />
+            </div>
+            <input type="text" placeholder="Enter your email" />
+            <div className="icon2">
+              <AiOutlineSend />
+              </div>
+          </div>
         </div>
-
       </div>
-
 
       <div className="footerin2">
         <h3>© Copyright 2023 AllenGrocery, Inc. All rights Reserved.</h3>
