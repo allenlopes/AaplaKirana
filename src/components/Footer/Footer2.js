735f27358cd6e2c666747d1a50e03022f312ddd4
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-26 23:55:10 +0530

    In src/components/Footer/Footer2.js file modified

---
+++ src/components/Footer/Footer2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer2 = () => {
+  return (
+    <div className="footer">
+      <div className="footerin1"></div>
+      <div className="footerin2"></div>
+    </div>
+  );
+}
+
+export default Footer2
