9a7781c67d33dccbeb78421ea55de1a917d62743
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-30 00:21:51 +0530

    In src/App.js file modified by adding the route of particular product page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ProductPage from './pages/Product/ProductPage';
 
 const App = () => {
   return (
@@ -13,6 +14,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/product/:prodid"
+          element={
+            <ProductPage />
+          }
+        />
         <Route path="*" element={<div>
           <h1>404 Not Found :(</h1>
         </div>} />
