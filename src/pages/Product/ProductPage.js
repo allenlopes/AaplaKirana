7008fb43fad3de090bd39a87c7a515a2902ca344
sudoallenlopes23 <allenlopes10@gmail.com>
2023-07-31 23:53:27 +0530

    In src/pages/Product/ProductPage.js file modified

--- src/pages/Product/ProductPage.js
+++ src/pages/Product/ProductPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductPage = () => {
+  const {prodid} = useParams()
   return (
-    <div>ProductPage</div>
+    <div>
+      <h1>Product id is - {prodid}</h1>
+    </div>
   )
 }
 
